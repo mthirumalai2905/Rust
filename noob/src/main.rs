34cc14ecfa5577981406b92825955de5f55bded5
Thirumalainambi <thiru@thiru.com>
2024-04-22 01:01:22 +0530

    added Variables,constants, variable shadowing

--- noob/src/main.rs
+++ noob/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // let  x   = 4;
+    // println!("x is : {}", x);
+    // {
+    //     let x = x - 1;
+    //     println!("x is: {}", x);
+    // }
+    // let x =  5;
+    // println!("x is: {}", x);
+
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
